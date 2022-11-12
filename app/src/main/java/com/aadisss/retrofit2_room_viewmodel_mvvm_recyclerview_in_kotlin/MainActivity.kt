d7138aca02bafed32b290042bd1e53bd73391f26
Adeel Nasir <adeel.nasir@myant.ca>
2022-11-12 16:16:10 +0000

    first commit

---
+++ app/src/main/java/com/aadisss/retrofit2_room_viewmodel_mvvm_recyclerview_in_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aadisss.retrofit2_room_viewmodel_mvvm_recyclerview_in_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/aadisss/retrofit2_room_viewmodel_mvvm_recyclerview_in_kotlin/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.aadisss.retrofit2_room_viewmodel_mvvm_recyclerview_in_kotlin.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    val BASE_URL = "https://api.github.com/search"
+
+    @Provides
+    @Singleton
+    fun getRetrofitInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
